c121b47d3973c6d5feb9f20f94fb1211de215842
yshimizu-cq <y.shimizu@caraquri.com>
2020-06-19 16:58:50 +0900

    Create signup view and build a relationship with login view

--- BookManager_iOS/Login/LoginViewController.swift
+++ BookManager_iOS/Login/LoginViewController.swift
@@ -8,27 +8,26 @@
 
 import UIKit
 
+
 //  ログイン画面
 class LoginViewController: UIViewController {
 
-    //  メールアドレス入力用
     @IBOutlet weak var mailTextField: UITextField!
-    
-    //  パスワード入力用
     @IBOutlet weak var passwordTextField: UITextField!
     
-    //  画面が読み込まれた際に実行すること
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
-    //  ログインボタンをタップした時の処理
     @IBAction func loginButtonTapped(_ sender: UIButton) {
     }
     
-    //  新規アカウント作成ボタンをタップした時の処理
     @IBAction func signupButtonTapped(_ sender: UIButton) {
+
+        let storyboard: UIStoryboard = R.storyboard.signup()
+        let nextView = storyboard.instantiateInitialViewController()
+        present(nextView!, animated: true, completion: nil)
     }
     
 
---
+++ BookManager_iOS/Singup/SignupViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SignupViewController.swift
+//  BookManager_iOS
+//
+//  Created by 清水雄大 on 2020/06/19.
+//  Copyright © 2020 Yuta Shimizu. All rights reserved.
+//
+
+import UIKit
+
+//  サインアップ画面
+class SignupViewController: UIViewController {
+    
+    @IBOutlet weak var mailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordConfirmationTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func returnButtonTapped(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func signupButtonTapped(_ sender: UIButton) {
+    }
+}
+
