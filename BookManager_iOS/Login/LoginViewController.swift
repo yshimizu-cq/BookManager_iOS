3864b01f015c378835dcb1594043d04d14f3866d
yshimizu-cq <y.shimizu@caraquri.com>
2020-06-19 12:32:17 +0900

    Create login view parts and connect to LoginViewController

--- BookManager_iOS/Login/LoginViewController.swift
+++ BookManager_iOS/Login/LoginViewController.swift
@@ -8,13 +8,29 @@
 
 import UIKit
 
+//  ログイン画面
 class LoginViewController: UIViewController {
 
+    //  メールアドレス入力用
+    @IBOutlet weak var mailTextField: UITextField!
+    
+    //  パスワード入力用
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    //  画面が読み込まれた際に実行すること
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
+    
+    //  ログインボタンをタップした時の処理
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+    }
+    
+    //  新規アカウント作成ボタンをタップした時の処理
+    @IBAction func signupButtonTapped(_ sender: UIButton) {
+    }
+    
 
 }
 
