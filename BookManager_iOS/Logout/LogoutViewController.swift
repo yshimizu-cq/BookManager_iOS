158d4cd1c1492efdcc1dbfa64c455134f2b22979
yshimizu-cq <y.shimizu@caraquri.com>
2020-06-22 13:59:00 +0900

    Rename to return button of navigation bar

---
+++ BookManager_iOS/Logout/LogoutViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LogoutViewController.swift
+//  BookManager_iOS
+//
+//  Created by 清水雄大 on 2020/06/22.
+//  Copyright © 2020 Yuta Shimizu. All rights reserved.
+//
+
+import UIKit
+
+class LogoutViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    
+}
